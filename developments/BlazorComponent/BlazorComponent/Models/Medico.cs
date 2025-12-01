720dfbfe71a4c16a8381a068ff3160c4cef2de12
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-12-01 00:48:37 -0300

    feat: adição do arquivo Medico.cs

---
+++ developments/BlazorComponent/BlazorComponent/Models/Medico.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorComponent.Models
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nome deve ser fornecido")]
+        [MinLength(3, ErrorMessage = "Nome deve conter no mínimo 3 caracteres")]
+        [MaxLength(50, ErrorMessage = "Nome deve conter no máximo 50 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "CRM deve ser fornecido")]
+        public string Crm { get; set; }
+    }
+}
