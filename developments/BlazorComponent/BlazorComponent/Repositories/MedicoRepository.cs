486f5e9cf45cbc2a320401dbf8592226d86d8986
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-12-01 00:48:41 -0300

    feat: adição do arquivo MedicoRepository.cs

---
+++ developments/BlazorComponent/BlazorComponent/Repositories/MedicoRepository.cs
@@ -0,0 +1,19 @@
+﻿using BlazorComponent.Models;
+
+namespace BlazorComponent.Repositories
+{
+    public class MedicoRepository : IMedicoRepository
+    {
+        public async Task<List<Medico>> GetAllAsync()
+        {
+            return new List<Medico>
+            {
+                new Medico{ Id = 1, Nome = "Pedro Silva", Crm = "ABC123" },
+                new Medico{ Id = 2, Nome = "Antonio Garcia", Crm = "DEF123" },
+                new Medico{ Id = 3, Nome = "Mateus Costa", Crm = "GHI123" },
+                new Medico{ Id = 4, Nome = "Alberto Roberto", Crm = "JKL123" },
+                new Medico{ Id = 5, Nome = "Pedro Menezes", Crm = "MNO123" },
+            };
+        }
+    }
+}
